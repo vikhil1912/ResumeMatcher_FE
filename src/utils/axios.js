ebcdccf37ebb28f170820da1c9e2e3792891294b
sri vikhil <vikhil1912@gmail.com>
2025-08-04 23:56:34 +0530

    add cred true to axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://resume-matcher-pi.vercel.app/api/",
-  credintials: "include",
+  // credintials: "include",
   withCredentials: true,
 });
 
